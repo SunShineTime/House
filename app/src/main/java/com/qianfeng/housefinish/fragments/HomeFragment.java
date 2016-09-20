96724f38460331c57f81dc78b2bf9dc314d4f54e
Androidshuying <937673596@qq.com>
2016-09-20 15:20:16 +0800

    新建了几个文件夹

--- app/src/main/java/com/qianfeng/housefinish/fragments/HomeFragment.java
+++ app/src/main/java/com/qianfeng/housefinish/fragments/HomeFragment.java
@@ -3,11 +3,17 @@ package com.qianfeng.housefinish.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.qianfeng.housefinish.R;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.x;
 
 /**
  * 首页
@@ -15,6 +21,7 @@ import com.qianfeng.housefinish.R;
 public class HomeFragment extends BaseFragment {
 
     public static final String TAG = HomeFragment.class.getSimpleName();
+    private ListView mListView;
 
     @Nullable
     @Override
@@ -31,5 +38,30 @@ public class HomeFragment extends BaseFragment {
 
     private void initView() {
 
+        mListView = (ListView)layout.findViewById(R.id.homefragment_listview);
+
+
+        RequestParams params = new RequestParams("http://portal-web.zhaidou.com/decorate/getChangeCases.action?pageSize=20&pageNo=1");
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                Log.e(TAG, "onSuccess: "+result );
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e(TAG, "onError: " );
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+                Log.e(TAG, "onCancelled: " );
+            }
+
+            @Override
+            public void onFinished() {
+                Log.e(TAG, "onFinished: " );
+            }
+        });
     }
 }
