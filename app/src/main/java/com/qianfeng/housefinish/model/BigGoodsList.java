8a19773dfd2c5b1c392e568f8112d5282add18f4
魑魅魑魅 <1129679783@qq.com>
2016-09-20 20:56:15 +0800

    shangcheng

---
+++ app/src/main/java/com/qianfeng/housefinish/model/BigGoodsList.java
@@ -0,0 +1,16 @@
+package com.qianfeng.housefinish.model;
+
+/**
+ * Created by chengchao on 2016/9/20.
+ */
+public class BigGoodsList {
+    private GoodsList data;
+
+    public GoodsList getData() {
+        return data;
+    }
+
+    public void setData(GoodsList data) {
+        this.data = data;
+    }
+}
---
+++ app/src/main/java/com/qianfeng/housefinish/model/GoodsList.java
@@ -0,0 +1,18 @@
+package com.qianfeng.housefinish.model;
+
+import java.util.List;
+
+/**
+ * Created by chengchao on 2016/9/20.
+ */
+public class GoodsList {
+    private List<Goods> themeList;
+
+    public List<Goods> getThemeList() {
+        return themeList;
+    }
+
+    public void setThemeList(List<Goods> themeList) {
+        this.themeList = themeList;
+    }
+}
