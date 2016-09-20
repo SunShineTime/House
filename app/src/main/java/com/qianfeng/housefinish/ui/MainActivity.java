3e886c36dc07b71bef0c09909927c96e17a5f0c5
SunShineWD <894690755@qq.com>
2016-09-20 13:59:06 +0800

    init

--- app/src/main/java/com/qianfeng/housefinish/ui/MainActivity.java
+++ app/src/main/java/com/qianfeng/housefinish/ui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.qianfeng.housefinish.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
 import android.widget.RadioGroup;
 
 import com.qianfeng.housefinish.R;
@@ -27,18 +30,22 @@ import java.lang.reflect.InvocationTargetException;
  */
 
 @ContentView(R.layout.activity_main)
-public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedChangeListener {
+public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedChangeListener,Handler.Callback {
 
+    private static final int EXIT = 100;
+    private static final long DELAY_TIME = 3000;
     @ViewInject(R.id.main_radiogroup)
     private RadioGroup mRadioGroup;
     private Fragment mFragment;
-
+    private boolean isExit;
+    private Handler mHandler=new Handler(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
         initView();
+        ToastUtil.init(this);
     }
 
     private void initView() {
@@ -48,7 +55,7 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
         mFragment = new HomeFragment();
-        transaction.add(R.id.main_fragment,mFragment,HomeFragment.TAG);
+        transaction.add(R.id.main_fragment, mFragment, HomeFragment.TAG);
         transaction.commit();
     }
 
@@ -56,19 +63,19 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
     public void onCheckedChanged(RadioGroup group, int checkedId) {
         switch (checkedId) {
             case R.id.main_fragment_home:
-                switchPages(HomeFragment.TAG,HomeFragment.class);
+                switchPages(HomeFragment.TAG, HomeFragment.class);
                 break;
             case R.id.main_fragment_goods:
-                switchPages(GoodsFragment.TAG,GoodsFragment.class);
+                switchPages(GoodsFragment.TAG, GoodsFragment.class);
                 break;
             case R.id.main_fragment_magic:
-                switchPages(MagicFragment.TAG,MagicFragment.class);
+                switchPages(MagicFragment.TAG, MagicFragment.class);
                 break;
             case R.id.main_fragment_cart:
-                switchPages(CartFragment.TAG,CartFragment.class);
+                switchPages(CartFragment.TAG, CartFragment.class);
                 break;
             case R.id.main_fragment_account:
-                switchPages(AccountFragment.TAG,AccountFragment.class);
+                switchPages(AccountFragment.TAG, AccountFragment.class);
                 break;
         }
 
@@ -76,17 +83,18 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
 
     /**
      * 切换页面效果
+     *
      * @param tag 添加碎片的标记
      * @param cls 添加碎片的Class
      */
-    private void switchPages(String tag,Class<? extends BaseFragment> cls){
+    private void switchPages(String tag, Class<? extends BaseFragment> cls) {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction transaction = fm.beginTransaction();
         transaction.hide(mFragment);
-        mFragment=fm.findFragmentByTag(tag);
-        if (mFragment!=null) {
+        mFragment = fm.findFragmentByTag(tag);
+        if (mFragment != null) {
             transaction.show(mFragment);
-        }else {
+        } else {
             try {
                 mFragment = cls.getConstructor().newInstance();
             } catch (InstantiationException e) {
@@ -98,9 +106,36 @@ public class MainActivity extends BaseActivity implements RadioGroup.OnCheckedCh
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             }
-            transaction.add(R.id.main_fragment,mFragment,tag);
+            transaction.add(R.id.main_fragment, mFragment, tag);
         }
         transaction.commit();
     }
 
+    //按返回键退出
+
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // 判断按下的按钮是否是返回键
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (!isExit) {
+                ToastUtil.toast("再按一次退出");
+                isExit = true;
+                mHandler.sendEmptyMessageDelayed(EXIT, DELAY_TIME);
+                return true;
+            }
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case EXIT:
+                isExit = false;
+                break;
+        }
+        return false;
+    }
 }
---
+++ app/src/main/java/com/qianfeng/housefinish/ui/ToastUtil.java
@@ -0,0 +1,22 @@
+package com.qianfeng.housefinish.ui;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * 吐司工具类
+ */
+public class ToastUtil {
+
+    private static Toast toast;
+
+    public static void init(Context context){
+        toast = Toast.makeText(context,"",Toast.LENGTH_SHORT);
+    }
+
+    public static void toast(String conent){
+        toast.setText(conent);
+        toast.show();
+    }
+
+}
