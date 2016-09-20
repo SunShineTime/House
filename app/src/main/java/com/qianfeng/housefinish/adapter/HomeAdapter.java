2adf9cc9797bb4d35171929392073f210f74d119
Androidshuying <937673596@qq.com>
2016-09-20 15:21:51 +0800

    新建了几个文件夹

---
+++ app/src/main/java/com/qianfeng/housefinish/adapter/HomeAdapter.java
@@ -0,0 +1,33 @@
+package com.qianfeng.housefinish.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 徐余璟 on 2016/9/20.
+ */
+public class HomeAdapter extends BaseAdapter {
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
