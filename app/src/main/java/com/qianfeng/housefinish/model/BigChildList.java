a7c12d453cc983f48c43446b005fd04e4ca88cdb
Androidshuying <937673596@qq.com>
2016-09-21 15:57:43 +0800

    徐影

---
+++ app/src/main/java/com/qianfeng/housefinish/model/BigChildList.java
@@ -0,0 +1,18 @@
+package com.qianfeng.housefinish.model;
+
+import java.util.List;
+
+/**
+ * Created by 徐余璟 on 2016/9/21.
+ */
+public class BigChildList {
+    List<ChildList>children;
+
+    public List<ChildList> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<ChildList> children) {
+        this.children = children;
+    }
+}
